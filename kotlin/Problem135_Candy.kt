b9750ecc1a82754868e3d44c388825b5b44a4d8c
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-07-16 20:54:12 -0600

    Update Problem135_Candy.kt

--- kotlin/Problem135_Candy.kt
+++ kotlin/Problem135_Candy.kt
@@ -1,4 +1,7 @@
 /**
+// LeetCode 135 - Candy
+// https://leetcode.com/problems/candy/
+
 There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
 
 You are giving candies to these children subjected to the following requirements:
