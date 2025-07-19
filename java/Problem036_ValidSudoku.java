3e7a9ec29634f09e440db00e5f872c1f27a5197f
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-07-18 18:44:58 -0600

    Update Problem036_ValidSudoku.java

--- java/Problem036_ValidSudoku.java
+++ java/Problem036_ValidSudoku.java
@@ -18,7 +18,22 @@ class Solution {
                     continue;
                 else
                     return false;
+            }
+        }
 
+        for (int rowBlock=0; rowBlock<3; rowBlock++) {
+            for (int colBlock=0; colBlock<3; colBlock++) {
+                for (int row=0; row<3; row++) {
+                    for (int col=0; col<3; col++) {
+                        char value = board[rowBlock*3 +row][colBlock*3 +col];
+
+                        if (validBlock(value))
+                            continue;
+                        else
+                            return false;
+                    }
+                }
+                block.clear();
             }
         }
 
@@ -39,5 +54,11 @@ class Solution {
         if (hasNumber(value)) 
           return col.add(value);
         return true;        
+    }
+
+    private boolean validBlock(char value) {
+        if (hasNumber(value))
+            return block.add(value);
+        return true;
     }
 }
