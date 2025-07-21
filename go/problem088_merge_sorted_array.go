2a741bbcd126949707636b3626c29e0f8841e027
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-07-21 16:28:12 -0300

    Update problem088_merge_sorted_array.go

--- go/problem088_merge_sorted_array.go
+++ go/problem088_merge_sorted_array.go
@@ -5,8 +5,8 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
    index2 := n-1
    total := m+n-1
 
-   for index1 >= 0 && index2 >= 0 {
-    if nums1[index1] >= nums2[index2] {
+   for index2 >= 0 {
+    if index1 >0 && nums1[index1] >= nums2[index2] {
         nums1[total] = nums1[index1]
         index1--
     } else {
@@ -14,11 +14,5 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
         index2--
     }
     total--
-   }
-
-   for index2 >=0 {
-    nums1[total] = nums2[index2]
-    total--
-    index2--
    }
 }
