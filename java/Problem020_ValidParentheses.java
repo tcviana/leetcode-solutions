4a2e336ae603d02309053ab79dc7510e80017acf
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-07-23 22:56:19 -0300

    Update Problem020_ValidParentheses.java

--- java/Problem020_ValidParentheses.java
+++ java/Problem020_ValidParentheses.java
@@ -1,2 +1,36 @@
 // https://leetcode.com/problems/valid-parentheses
 
+class Solution {
+
+    Map<Character, Character> pairCloseParentheses = Map.of(')','(',']','[','}','{');
+    List<Character> stack = new ArrayList<>();
+
+    public boolean isValid(String s) {
+
+        for (char c : s.toCharArray()) {
+            if (openParentheses(c)) {
+                stack.add(c);
+                continue;
+            } 
+            if (validClose(c)) {
+                stack.remove(stack.size()-1);
+                continue;
+            } 
+            return false;
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean openParentheses(char c) {
+        return !pairCloseParentheses.containsKey(c);
+    }
+
+    private boolean validClose(char c) {
+        int size = stack.size();
+
+        if (size > 0) {
+            return stack.get(size-1) == pairCloseParentheses.get(c);
+        }
+        return false;
+    }
+}
