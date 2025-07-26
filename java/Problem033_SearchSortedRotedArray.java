9a9561bd61a2d2912de53364e1e19f1ccd8eea99
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-07-26 14:12:43 -0300

    Update Problem033_SearchSortedRotedArray.java

--- java/Problem033_SearchSortedRotedArray.java
+++ java/Problem033_SearchSortedRotedArray.java
@@ -1,2 +1,19 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
+class Solution {
+    public int search(int[] nums, int target) {
+        int right = nums.length -1;
+
+        for (int i=0; i<=right; i++) {
+            if (nums[i] == target) {
+                return i;
+            } else if (nums[right] == target) {
+                return right;
+            }
+            right--;
+        }
+
+        return -1;
+        
+    }
+}
