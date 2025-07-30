b68596bef9ad72fe468c468b36d709d1fa6f5c1d
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-07-30 00:17:10 -0300

    Update Problem141_LinkedListCycle.java

--- java/Problem141_LinkedListCycle.java
+++ java/Problem141_LinkedListCycle.java
@@ -1,2 +1,19 @@
 // https://leetcode.com/problems/linked-list-cycle/
 
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true; // ciclo detectado
+        }
+
+        return false; // chegou no final, não há ciclo
+    }
+}
