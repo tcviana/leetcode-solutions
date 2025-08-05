86c5d8cf0f99b2f32e9bf202c22c4b862fd9390d
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-04 21:21:08 -0300

    Update problem_012_integer_to_roman.go

--- go/problem_012_integer_to_roman.go
+++ go/problem_012_integer_to_roman.go
@@ -4,14 +4,14 @@ func intToRoman(num int) string {
         values := []int {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
         symbols := []string {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
-        var roman string
+        var roman strings.Builder
 
         for i:=0; i<len(values); i++ {
             for num >= values[i] {
                 num -= values[i]
-                roman += symbols[i]
+                roman.WriteString(symbols[i])
             }
         }
 
-        return roman
+        return roman.String()
 }
