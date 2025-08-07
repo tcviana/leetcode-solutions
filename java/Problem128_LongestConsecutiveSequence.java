7ddfac74cd201eb38ca311b72f578cbb4254b9c1
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-06 23:36:07 -0300

    Update Problem128_LongestConsecutiveSequence.java

--- java/Problem128_LongestConsecutiveSequence.java
+++ java/Problem128_LongestConsecutiveSequence.java
@@ -2,6 +2,9 @@
 
 class Solution {
     public int longestConsecutive(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+
         Arrays.sort(nums);
         int count = 1;
         int max = 1;
@@ -9,7 +12,7 @@ class Solution {
         for (int i=1; i<nums.length; i++) {
             if (nums[i] == nums[i-1])
                 continue;
-                
+
             if (nums[i] == (nums[i-1]+1)) {
                 count++;
                 if (count > max) {
