9520837daab2d57abc4829a9769bc722bfd1a764
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-06 23:58:43 -0300

    Update problem128_longest_consecutive_sequence.go

--- go/problem128_longest_consecutive_sequence.go
+++ go/problem128_longest_consecutive_sequence.go
@@ -1,2 +1,25 @@
 // https://leetcode.com/problems/longest-consecutive-sequence
 
+func longestConsecutive(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    sort.Slice(nums, func(i,j int) bool { return nums[i]<nums[j] })
+    count := 1
+    result := 1
+
+    for i := 1; i<len(nums); i++ {
+        if nums[i] == nums[i-1] {
+            continue
+        }
+        if (nums[i] == (nums[i-1])+1) {
+            count++
+
+            result = max(count,result)
+        } else {
+            count = 1
+        }
+    }
+
+    return result
+}
