22b582b69738d3ecc142d7fc812cfcde2e23b832
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-08 17:21:00 -0600

    Update problem088_merge_sorted_array.go

--- go/problem088_merge_sorted_array.go
+++ go/problem088_merge_sorted_array.go
@@ -1,18 +1,10 @@
 // https://leetcode.com/problems/merge-sorted-array/
 
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-   index1 := m-1
-   index2 := n-1
-   total := m+n-1
-
-   for index2 >= 0 {
-    if index1 >=0 && nums1[index1] >= nums2[index2] {
-        nums1[total] = nums1[index1]
-        index1--
-    } else {
-        nums1[total] = nums2[index2]
-        index2--
+    if n>0 {
+        for i:=0; i<n; i++ {
+            nums1[m+i]=nums2[i]
+        }
+        sort.Slice(nums1, func(i,j int) bool {return nums1[i]<nums1[j]})
     }
-    total--
-   }
 }
