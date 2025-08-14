009b9d601a437c8e86450c0127375967de3ed249
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-14 00:01:05 -0300

    Update problem006_zigzag_conversion.go

--- go/problem006_zigzag_conversion.go
+++ go/problem006_zigzag_conversion.go
@@ -1,2 +1,29 @@
 // https://leetcode.com/problems/zigzag-conversion/
 
+func convert(s string, numRows int) string {
+
+    if numRows == 1 {
+        return s
+    }
+
+    m := make(map[int]string, numRows)
+    row := 0
+    direction := -1
+
+    for _, c := range s {
+        m[row] += string(c)
+        
+        if (row == 0 || row == numRows-1) {
+            direction *= -1
+        }
+        row += direction
+    }
+
+    var sb strings.Builder
+
+    for i:=0; i<numRows; i++ {
+        sb.WriteString(m[i])
+    }
+
+    return sb.String()
+}
