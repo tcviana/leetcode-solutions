dc05c4af4605bba463edf228a46664c16e7fcca5
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-14 21:04:17 -0300

    Update Problem006_ZigZagConversion.java

--- java/Problem006_ZigZagConversion.java
+++ java/Problem006_ZigZagConversion.java
@@ -4,9 +4,9 @@ class Solution {
     public String convert(String s, int numRows) {
         if (numRows == 1 || s.length() <= numRows) return s;
 
-        StringBuilder[] rows = new StringBuilder[numRows+1];
+        StringBuilder[] rows = new StringBuilder[numRows];
 
-        for (int i=0; i<=numRows; i++) {
+        for (int i=0; i<numRows; i++) {
             rows[i] = new StringBuilder();
         }
         int increment = -1;
@@ -18,13 +18,15 @@ class Solution {
             if (row == 0 || row == (numRows-1)) {
                 increment *= -1;
             }
+            row += increment;
         }
 
+        StringBuilder result = new StringBuilder();
         for (int i=0; i<numRows; i++) {
-            rows[numRows].append(rows[i]);
+            result.append(rows[i]);
         }
 
-        return rows[numRows].toString();
+        return result.toString();
 
     }
 }
