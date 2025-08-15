a5a2af060485799a3104ef0bce9cac3ba136403a
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-14 21:42:02 -0300

    Create problem0_longest_common_prefix.go

---
+++ go/problem0_longest_common_prefix.go
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 1 {
+        return strs[0]
+    }
+
+    
+
+    return ""
+}
