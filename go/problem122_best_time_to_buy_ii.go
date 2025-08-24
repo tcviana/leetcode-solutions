7a329c238761c9dfb49fee533c4f457443083e58
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-08-24 20:03:57 -0300

    Create problem122_best_time_to_buy_ii.go

---
+++ go/problem122_best_time_to_buy_ii.go
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+func maxProfit(prices []int) int {
+    sum := 0
+
+    for i:=1; i<len(prices); i++ {
+        if prices[i] > prices[i-1] {
+            sum += prices[i] - prices[i-1]
+        }
+    }
+
+    return sum
+    
+}
