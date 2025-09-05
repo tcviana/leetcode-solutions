473e2a97bc221ec7f458a8c547e966c557020c30
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-09-04 21:30:55 -0300

    Create Problem202_HappyNumber.java

---
+++ java/Problem202_HappyNumber.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/happy-number/
+
+class Solution {
+    public boolean isHappy(int n) {
+        int size = n.length();
+
+        while (size > 1) {
+            int sum = 0;
+
+            for (int i=0; i<size; i++) {
+                
+            }
+            size = n.length();
+        }
+
+        return n==1;
+
+        
+    }
+}
