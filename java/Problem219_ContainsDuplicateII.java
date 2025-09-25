d9e4a90287eb02b2c9faceda3da68b8bbf7bdc7a
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-09-25 15:13:34 -0300

    Update Problem219_ContainsDuplicateII.java

--- java/Problem219_ContainsDuplicateII.java
+++ java/Problem219_ContainsDuplicateII.java
@@ -2,6 +2,18 @@
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++) {
+            int value = nums[i];
+            if (map.containsKey(value)) {
+                int dif = i - map.get(value);
+
+                if (dif <= k) return true;
+            }
+            map.put(value, i);
+        }
+
+        return false;
     }
 }
