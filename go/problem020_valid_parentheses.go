aee943b24db5e193bef2f9adc75677535a4af6d5
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-12-23 22:18:17 -0300

    Update problem020_valid_parentheses.go

--- go/problem020_valid_parentheses.go
+++ go/problem020_valid_parentheses.go
@@ -1,5 +1,30 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 func isValid(s string) bool {
-    
+    if len(s)%2 !=0 {
+            return false
+    }
+    bracket := map[rune]rune {
+        '(':')',
+        '{':'}',
+        '[':']',
+    }
+
+    var stack []rune
+
+    for _, c := range s {
+
+        if closeBracket, ok := bracket[c]; ok {
+            stack = append(stack, closeBracket)
+        } else {
+            if len(stack) == 0 || stack[len(stack)-1] != c {
+                return false
+            }
+            stack = stack[:len(stack)-1]
+        }
+
+    }
+    return len(stack) == 0    
+
 }
+
