84f755cf0100adf7ddd516f24689f26725e3e226
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2025-12-26 23:06:20 -0300

    Update Problem045_JumpGame2.java

--- java/Problem045_JumpGame2.java
+++ java/Problem045_JumpGame2.java
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/jump-game-ii/
 
-func jump(nums []int) int {
-    
+class Solution {
+    public int jump(int[] nums) {
+        int size = nums.length;
+        int jumps = 0;
+
+        if (size <= 1) return jumps;
+
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < size - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;            
+                currentEnd = farthest; 
+
+                if (currentEnd >= nums.length - 1) break;
+            }
+        }
+
+        return jumps;
+    }
 }
