c19ba5835907367370163c015b630ec6e12a8af8
Tiago Coutinho Viana <42697995+tcviana@users.noreply.github.com>
2026-01-18 23:04:37 -0300

    Update Problem274_HIndex.java

--- java/Problem274_HIndex.java
+++ java/Problem274_HIndex.java
@@ -2,6 +2,18 @@
 
 class Solution {
     public int hIndex(int[] citations) {
-        
+        Arrays.sort(citations);
+        int size = citations.length;
+
+        for (int x=0; x<size; x++) {
+            int citation = citations[x];
+            int article = size-x;
+
+            if (citation >= article) {
+                return article;
+            }
+        }
+
+        return 0;
     }
 }
